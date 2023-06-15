0fd3cee86a53b6dcc1e5ea9f5c41cfecfe60517d
lanowde <117498666+lanowde@users.noreply.github.com>
2023-06-15 20:37:44 +0530

    support for python 3.11

--- loader/__init__.py
+++ loader/__init__.py
@@ -1,1 +1,20 @@
 __version__ = "5.0"
+
+
+# for python 3.11
+# https://t.me/UserGeSpam/548736
+def fix_python311():
+    from platform import python_version_tuple
+
+    py_version = map(lambda i: int(i), python_version_tuple()[:2])
+    if tuple(py_version) > (3, 9):
+        # maintain the import order
+        from collections.abc import MutableMapping
+        from collections.abc import Container
+        import collections
+
+        setattr(collections, "MutableMapping", MutableMapping)
+        setattr(collections, "Container", Container)
+
+
+fix_python311()
